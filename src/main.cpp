7b710764a1b128662f7296bcefeabfa181971658
Sergwest <39670193+AFK1@users.noreply.github.com>
2021-08-06 00:11:32 +0300

    Update main.cpp
    aaa

--- src/main.cpp
+++ src/main.cpp
@@ -1,104 +1,75 @@
-
 #include<iostream>
+#include<stdio.h>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 #include<cmath>
+#include <fstream>
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
+#include <stdint.h>
 
-float x = 0;
-float y = 0;
-int speed = 1;
 
-int movement;
+struct Farbfeld_header {
+	uint64_t magic;
+	uint32_t w;
+	uint32_t h;
+};
 
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
-"layout (location = 1) in vec2 TexCoord;\n"
+"layout (location = 1) in vec2 aTexcord;\n"
 "uniform vec4 rot; \n"
-"uniform vec2 cmmove; \n"
 "uniform float rot2; \n"
-"out vec2 texcrd; \n"
+"out vec2 Texcord;\n"
 "void main()\n"
 "{\n"
-"   texcrd = TexCoord"
 "   float x = aPos.x*rot.x/2+aPos.x;\n"
 "   float y = aPos.y*rot.y/2+aPos.y;\n"
 "   float z = aPos.z*rot.z/2+aPos.z;\n"
-"   if (rot2 < 360){\n"
-"   gl_Position = vec4(x*cos(rot2/57.6)+z*sin(rot2/57.6), "
+"   float cs = cos(rot2/57.6),sn = sin(rot2/57.6);\n"  
+"   Texcord = aTexcord;\n"
+"   if (rot2 <= 360){\n"
+"   gl_Position = vec4(x*cs+z*sn, "
 "					   y, "
-"					   z*-1*sin(rot2/57.6)+z*cos(rot2/57.6), "
+"					   z*-1*sn+z*cs, "
 "	1.0);\n"
 "	}else{\n"
-"   if (rot2 < 720){\n"
-"   gl_Position = vec4(x*cos(rot2/57.6)+y*-1*sin(rot2/57.6), "
-"					   x*sin(rot2/57.6)+y*cos(rot2/57.6), "
+"   if (rot2 <= 720){\n"
+"   gl_Position = vec4(x*cs+y*-1*sn, "
+"					   x*sn+y*cs, "
 "					   z, "
 "	1.0);\n"
 "	}\n"
 "   else {\n"
 "   gl_Position = vec4(x, "
-"					   y*cos(rot2/57.6)+z*-1*sin(rot2/57.6), "
-"					   y*sin(rot2/57.6)+z*cos(rot2/57.6), "
+"					   y*cs+z*-1*sn, "
+"					   y*sn+z*cs, "
 "	1.0);\n"
 "	}}\n"
-//"	gl_Position = vec4(gl_Position.x + cmmove.x, gl_Position.y + cmmove.y, gl_Position.z, 1.0f);\n"
+//"	gl_Position = vec4(gl_Position.x + sin(gl_Position.y*400) ,gl_Position.yzw);\n"
 "}\0";
 const char* fragmentShaderSource = "#version 330 core\n"
-"in vec2 texcrd; \n"
+"in vec2 Texcord;\n"
+"uniform vec4 Color; \n"
+"uniform sampler2D texture;\n"
 "out vec4 FragColor;\n"
-"uniform sampler2D textur;\n"
-"uniform vec4 Color;\n"
 "void main()\n"
 "{\n"
-"   FragColor = texture(textur, texcrd) * Color;\n"
+"   FragColor = texture(texture, Texcord);\n" //vec4(Texcord.xy, Color.zw);\n"//
 "}\n\0";
 
-void movexy(float xp, float yp, float way) {
-	float tm = glfwGetTime();
-	float xl = xp;
-	float yl = yp;
-	while (abs(xp - xl + yp - yl) < way) {
-		xp += 0.1f * speed * (glfwGetTime()-tm);
-	}
-	glUniform2f(movement, x, y);
-}
 
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-	if (key == GLFW_KEY_D && action == GLFW_PRESS) {
-		movexy(0.1f, 0.0f, 1);
-	}
-	if (key == GLFW_KEY_A && action == GLFW_PRESS) {
-		movexy(-0.1f, 0.0f, 1);
-	}
-	if (key == GLFW_KEY_W && action == GLFW_PRESS) {
-		movexy(0.0f, 0.1f, 1);
-	}
-	if (key == GLFW_KEY_S && action == GLFW_PRESS) {
-		movexy(0.0f, -0.1f, 1);
-	}
-}
 
-GLuint VAO, VBO;
 int main()
 {
-	glfwSetTime(0);
-	// Initialize GLFW
+	glfwSetTime(0.0f);
 	glfwInit();
 
-	// Tell GLFW what version of OpenGL we are using 
-	// In this case we are using OpenGL 3.3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	// Tell GLFW we are using the CORE profile
-	// So that means we only have the modern functions
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
 	GLFWwindow* window = glfwCreateWindow(500, 500, "EBFSWT", NULL, NULL);
-	// Error check if the window fails to create
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -106,162 +77,165 @@ int main()
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
+
 	gladLoadGL();
 	glViewport(0, 0, 500, 500);
 
 
+	/*
+	// Итак, объясняю, как это работает:
+	
+	// Мы создаём системную переменную-ключ, которая позволяет нам открывать файлы.
+	// Не вдаваясь в подробности, это как VAO/VBO в OpenGL.
+	FILE* texture_f;
+	struct Farbfeld_header * texture_h = (struct Farbfeld_header*) malloc(sizeof(struct Farbfeld_header));
+	// задаём файл, который надо открыть.
+	// fopen( <Название_файла>, <Тип_открытия>).
+	// В данном случае файл - DIO.ff, а тип - r(ead only)
+	texture_f = fopen("DIO.ff", "r");
 
-	// Create Vertex Shader Object and get its reference
+	// Узнаём размер файла, для этого:
+	// Переходим в самый конец файла
+	// fseek(<Файл>, <смещение>, <Точка_начала_смещения>).
+	// У нас точка начала - это конец файла.
+	fseek(texture_f, 0L, SEEK_END);
+	// записываем позицию курсора.
+	size_t sz = ftell(texture_f);
+	// Возвращаем курсор на место.
+	rewind(texture_f);
+
+	// Выделяем баффер под текстуру.
+	const char* textureRaw = (const char*)malloc(sz*sizeof(char));
+	// Считываем файл.
+	fread((void*)texture_h, sizeof(struct Farbfeld_header), 1, texture_f);
+	fread((void*)textureRaw, sizeof(char), sz-sizeof(struct Farbfeld_header), texture_f);
+
+	printf("\nTexture: %s\nWidth: %ud\nHeight: %ud\n", texture_h->magic, texture_h->w, texture_h->h);
+
+	*/
+
 	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	// Attach Vertex Shader source to the Vertex Shader Object
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	// Compile the Vertex Shader into machine code
 	glCompileShader(vertexShader);
 
-	// Create Fragment Shader Object and get its reference
 	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	// Attach Fragment Shader source to the Fragment Shader Object
 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	// Compile the Vertex Shader into machine code
 	glCompileShader(fragmentShader);
 
-	// Create Shader Program Object and get its reference
 	GLuint shaderProgram = glCreateProgram();
-	// Attach the Vertex and Fragment Shaders to the Shader Program
 	glAttachShader(shaderProgram, vertexShader);
 	glAttachShader(shaderProgram, fragmentShader);
-	// Wrap-up/Link all the shaders together into the Shader Program
 	glLinkProgram(shaderProgram);
 
-	// Delete the now useless Vertex and Fragment Shader objects
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
+
+
+
+	GLfloat vertices[] =
+	{
+		-0.5f, 0.5f, 0.0f, 0.0f, 1.0f, // Upper corner
+		-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, // Lower left corner
+		0.5f, 0.5f, 0.0f, 1.0f, 1.0f, // Upper corner
+		0.5f, -0.5f, 0.0f, 1.0f, 0.0f // Lower right corner
+	}; 
+	GLuint verticesid[] =
+	{
+		0,1,2,
+		1,2,3
+	};
+
+	GLuint VAO, VBO, IBO;
+
+
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glGenBuffers(1, &IBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(verticesid), verticesid, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
 
 	GLuint texture;
 	glGenTextures(1, &texture);
 	glBindTexture(GL_TEXTURE_2D, texture);
-
-	stbi_set_flip_vertically_on_load(true);
-	int texwidth,texheigh,cnt;
-	unsigned char *data = stbi_load("asas.PNG", &texwidth, &texheigh, &cnt, 0);
-
-	glActiveTexture(GL_TEXTURE0);
-
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texwidth, texheigh, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	int width, height, numColCh;
+	unsigned char *image = stbi_load("container.jpg", &width, &height, &numColCh, 0);
+	printf("%d %d \n", width, numColCh);
+	glTexImage2D(GL_TEXTURE_2D, 0, numColCh == 3 ? GL_RGB : GL_RGBA, width, height, 0, numColCh == 3 ? GL_RGB : GL_RGBA, GL_UNSIGNED_BYTE, image);
+	
 	glGenerateMipmap(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	stbi_image_free(image);
 	glBindTexture(GL_TEXTURE_2D, 0);
-	stbi_image_free(data);
-
-	glUseProgram(shaderProgram);
-	// Sets the value of the uniform
-	glUniform1i(glGetUniformLocation(shaderProgram, "textur"), 0);
 
 
-	// Vertices coordinates
-	GLfloat vertices[] =
-	{
-		-0.5f, 0.5f, 0.0f, 0.0f, 1.0f, // Upper corner
-		0.5f, 0.5f, 0.0f, 1.0f, 1.0f, // Upper corner
-		0.5f, -0.5f, 0.0f, 1.0f, 0.0f, // Lower right corner
-		-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, // Lower left corner
-		-0.5f, 0.5f, 0.0f, 0.0f, 1.0f, // Upper corner
-		0.5f, 0.5f, 0.0f, 1.0f, 1.0f, // Upper corner
-		0.5f, -0.5f, 0.0f, 1.0f, 0.0f, // Lower right corner
-	};
-
-	// Create reference containers for the Vartex Array Object and the Vertex Buffer Object
-
-	// Generate the VAO and VBO with only 1 object each
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-
-	// Make the VAO the current Vertex Array Object by binding it
-	glBindTexture(GL_TEXTURE_2D, texture);
-	glBindVertexArray(VAO);
-
-	// Bind the VBO specifying it's a GL_ARRAY_BUFFER
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	// Introduce the vertices into the VBO
-
-
-	movement = glGetUniformLocation(shaderProgram, "cmmove");
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(GLfloat)));
-	glEnableVertexAttribArray(1);
-
-	// Bind both the VBO and VAO to 0 so that we don't accidentally modify the VAO and VBO we created
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
-
-	
-
-	glUniform2f(movement, x, y);
 	int vertexColorLocation = glGetUniformLocation(shaderProgram, "Color");
+	std::cout << glGetError() << std::endl;
 	int Magic = glGetUniformLocation(shaderProgram, "rot");
+	std::cout << glGetError() << std::endl;
 	int Magic2 = glGetUniformLocation(shaderProgram, "rot2");
-	float i = 0;
-	float j = 0;
-	// Main while loop
+	std::cout << glGetError() << std::endl;
+	float i = 0, j = 0;
+	std::cout << glGetError() << std::endl;
 	glUseProgram(shaderProgram);
+	std::cout << glGetError() << std::endl;
 	glEnable(GL_BLEND);
-	//glBlendEquation(GL_FUNC_ADD);
+	std::cout << glGetError() << std::endl;
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_COLOR);
+	std::cout << glGetError() << std::endl;
 	//GL_ONE_MINUS_CONSTANT_COLOR
-	glfwSetKeyCallback(window, key_callback);
-	glClearColor(0.0f, 0.06f, 0.12f, 1.0f);
+	glClearColor(0.0f, 0.0f, 0.1f, 1.0f);
+	float t = 0;
 	while (!glfwWindowShouldClose(window))
 	{
-		i = (i + 0.1f);
-		if (i > 360) i = i - 360;
-		j = (j + 0.2f);
-		if (j > 1080) j = j - 1080;
-		// Specify the color of the background
-		//((sin(glfwGetTime() * 10) / 2.0f) + 0.5f)
-		glUniform4f(vertexColorLocation, 0.0f, 1.0f, 0.0f, 1.0f);
-		glUniform4f(Magic, sin(i/57.6), cos(i / 57.6), 1.0f, 1.0f);
-		glUniform1f(Magic2, j);
-		// Clean the back buffer and assign the new color to it
+		i = (i + (glfwGetTime()-t)*40);j = (j + (glfwGetTime()-t)*80);if (i > 360) { i -= 360; }; if (j > 1080) { j -= 1080; }
+		t = glfwGetTime();
+		glUniform4f(vertexColorLocation, 0.0f, 1.0f, 0.0f, 1.0f);glUniform4f(Magic, sin(i / 57.6), cos(i / 57.6), 1.0f, 1.0f);glUniform1f(Magic2, j);
 		glClear(GL_COLOR_BUFFER_BIT);
-		// Tell OpenGL which Shader Program we want to use
-		// Bind the VAO so OpenGL knows to use it
+
+		glBindTexture(GL_TEXTURE_2D, texture);
 		glBindVertexArray(VAO);
-		// Draw the triangle using the GL_TRIANGLES primitive
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		glDrawArrays(GL_TRIANGLES, 2, 3);
-		// Swap the back buffer with the front buffer
-		glUniform4f(vertexColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
-		i = (i + 240);
-		if (i > 360) i = i - 360;
-		glUniform4f(Magic, sin(i / 57.6), cos(i / 57.6), 1.0f, 1.0f);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		glDrawArrays(GL_TRIANGLES, 2, 3);
-		glUniform4f(vertexColorLocation, 0.0f, 0.0f, 1.0f, 1.0f);
-		i = (i + 240);
-		if (i > 360) i = i - 360;
-		glUniform4f(Magic, sin(i / 57.6), cos(i / 57.6), 1.0f, 1.0f);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		glDrawArrays(GL_TRIANGLES, 2, 3);
-		i = (i + 240);
-		if (i > 360) i = i - 360;
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glUniform4f(vertexColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);i = (i + 240);if (i > 360) { i -= 360; };glUniform4f(Magic, sin(i / 57.6), cos(i / 57.6), 1.0f, 1.0f);
+
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		glUniform4f(vertexColorLocation, 0.0f, 0.0f, 1.0f, 1.0f);i = (i + 240);if (i > 360) { i -= 360; };glUniform4f(Magic, sin(i / 57.6), cos(i / 57.6), 1.0f, 1.0f); 
+
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		i = (i + 240);if (i > 360) { i -= 360; };
 		glfwSwapBuffers(window);
-		// Take care of all GLFW events
 		glfwPollEvents();
-		std::cout << "A" << std::endl;
 	}
 
-	// Delete all the objects we've created
+
+
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
 	glDeleteProgram(shaderProgram);
-	// Delete window before ending the program
 	glfwDestroyWindow(window);
-	// Terminate GLFW before ending the program
 	glfwTerminate();
 	return 0;
 }
