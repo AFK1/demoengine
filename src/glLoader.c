39e2673c7b31d12b7688ec8c96bac700097e37dc
Sergwest <39670193+AFK1@users.noreply.github.com>
2021-08-06 19:58:58 +0300

    sorted and added some elements in Loader

--- src/glLoader.c
+++ src/glLoader.c
@@ -3,19 +3,31 @@
 void
 glLoadExt()
 {
+
+  glAttachShader				= (PFNGLATTACHSHADERPROC)glfwGetProcAddress("glAttachShader");
+  glBindBuffer					= (PFNGLBINDBUFFERPROC)glfwGetProcAddress("glBindBuffer");
+  glBindVertexArray				= (PFNGLBINDVERTEXARRAYPROC)glfwGetProcAddress("glBindVertexArray");
+  glBindVertexArrays			= (PFNGLBINDVERTEXARRAYSPROC)glfwGetProcAddress("glBindVertexArrays");
+  glBufferData					= (PFNGLBUFFERDATAPROC)glfwGetProcAddress("glBufferData");
+  glCompileShader				= (PFNGLCOMPILESHADERPROC)glfwGetProcAddress("glCompileShader");
+  glCreateProgram				= (PFNGLCREATEPROGRAMPROC)glfwGetProcAddress("glCreateProgram");
+  glCreateShader				= (PFNGLCREATESHADERPROC)glfwGetProcAddress("glCreateShader");
+  glDeleteBuffers				= (PFNGLDELETEBUFFERSPROC)glfwGetProcAddress("glDeleteBuffers");
+  glDeleteShader				= (PFNGLDELETESHADERPROC)glfwGetProcAddress("glDeleteShader");
+  glDeleteProgram				= (PFNGLDELETEPROGRAMPROC)glfwGetProcAddress("glDeleteProgram");
+  glDeleteVertexArrays			= (PFNGLDELETEVERTEXARRAYSPROC)glfwGetProcAddress("glDeleteVertexArrays");
+  glEnableVertexAttribArray		= (PFNGLENABLEVERTEXATTRIBARRAYPROC)glfwGetProcAddress("glEnableVertexAttribArray");
+  glGenBuffers					= (PFNGLGENBUFFERSPROC)glfwGetProcAddress("glGenBuffers");
+  glGenerateMipmap				= (PFNGLGENERATEMIPMAPPROC)glfwGetProcAddress("glGenerateMipmap");
+  glGenVertexArrays				= (PFNGLGENVERTEXARRAYSPROC)glfwGetProcAddress("glGenVertexArrays");
+  glGetUniformLocation			= (PFNGLGETUNIFORMLOCATIONPROC)glfwGetProcAddress("glGetUniformLocation");
+  glLinkProgram					= (PFNGLLINKPROGRAMPROC)glfwGetProcAddress("glLinkProgram");
+  glShaderSource			    = (PFNGLSHADERSOURCEPROC)glfwGetProcAddress("glShaderSource");
   glUseProgram                  = (PFNGLUSEPROGRAMPROC)glfwGetProcAddress("glUseProgram");
-  glDeleteProgram               = (PFNGLUSEPROGRAMPROC)glfwGetProcAddress("glDeleteProgram");
-  glDeleteBuffers               = (PFNGLUSEPROGRAMPROC)glfwGetProcAddress("glDeleteBuffers");
-  glDeleteVertexArrays          = (PFNGLUSEPROGRAMPROC)glfwGetProcAddress("glDeleteVertexArrays");
-  glUniform1f                   = (PFNGLUSEPROGRAMPROC)glfwGetProcAddress("glUniform1f");
-  glUniform1i                   = (PFNGLUSEPROGRAMPROC)glfwGetProcAddress("glUniform1i");
-  glUniform2f                   = (PFNGLUSEPROGRAMPROC)glfwGetProcAddress("glUniform2f");
-  glUniform4f                   = (PFNGLUSEPROGRAMPROC)glfwGetProcAddress("glUniform4f");
-  glEnableVertexAttribArray     = (PFNGLUSEPROGRAMPROC)glfwGetProcAddress("glEnableVertexAttribArray");
-  glVertexAttribPointer         = (PFNGLUSEPROGRAMPROC)glfwGetProcAddress("glVertexAttribPointer");
-  glBufferData                  = (PFNGLUSEPROGRAMPROC)glfwGetProcAddress("glBufferData");
-  glBindBuffer                  = (PFNGLUSEPROGRAMPROC)glfwGetProcAddress("glBindBuffer");
-  glGenBuffers                  = (PFNGLUSEPROGRAMPROC)glfwGetProcAddress("glGenBuffers");
-  glCreateShader                = (PFNGLUSEPROGRAMPROC)glfwGetProcAddress("glCreateShader");
+  glUniform1f                   = (PFNGLUNIFORM1FPROC)glfwGetProcAddress("glUniform1f");
+  glUniform1i                   = (PFNGLUNIFORM1IPROC)glfwGetProcAddress("glUniform1i");
+  glUniform2f                   = (PFNGLUNIFORM2FPROC)glfwGetProcAddress("glUniform2f");
+  glUniform4f                   = (PFNGLUNIFORM4FPROC)glfwGetProcAddress("glUniform4f");
+  glVertexAttribPointer         = (PFNGLVERTEXATTRIBPOINTERPROC)glfwGetProcAddress("glVertexAttribPointer");
 
 }
