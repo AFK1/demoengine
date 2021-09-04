6ae4dfb550a235823ef1dbf64c87914a4c54d1e3
sergwest <ksendzov.sd@mail.ru>
2021-09-04 22:44:14 +0300

    changes

--- engine/modules/render/include/render.h
+++ engine/modules/render/include/render.h
@@ -5,6 +5,22 @@
 #include <GLFW/glfw3.h>
 #include <GL/gl.h>
 
-void render_module_init();
+struct arr_buf {
+	GLuint vao;
+	GLuint vbo;
+	GLuint ibo;
+};
+
+struct arr_buf* create_obj_st();
+struct arr_buf* create_obj(const void* vertices, const void* verticesid);
+void draw(float x, float y, struct arr_buf* arr);
+void shaders_load_st();
+void shaders_load(const char* vertexShaderSource, const char* fragmentShaderSource);
+void swap_buf(void* window);
+void clear_win();
+void render_init();
+void close_win(void* window);
 
 #endif // __DEMO_M_RENDER_H_
+
+
